8d24d5cd9a900f09ea841b93af7735c4151e309c
João Vitor Neves <jvneves_marq@hotmail.com>
2024-02-16 17:44:38 -0300

    first commit

---
+++ src/main/java/br/com/jvneves/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,11 @@
+package br.com.jvneves.gestao_vagas.modules.candidate.useCases;
+
+import br.com.jvneves.gestao_vagas.modules.candidate.CandidateEntity;
+
+public class CreateCandidateUseCase {
+
+  public CandidateEntity execute(CandidateEntity candidateEntity) {
+
+  }
+
+}
