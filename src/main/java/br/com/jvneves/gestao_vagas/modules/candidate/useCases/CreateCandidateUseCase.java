e1edb1ed9ad768c10879952ac03078492b7798a5
João Vitor Neves <jvneves_marq@hotmail.com>
2024-02-16 22:01:11 -0300

    Inserting simply error message and create CompanyEntity

--- src/main/java/br/com/jvneves/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/jvneves/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,11 +1,26 @@
 package br.com.jvneves.gestao_vagas.modules.candidate.useCases;
 
-import br.com.jvneves.gestao_vagas.modules.candidate.CandidateEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import br.com.jvneves.gestao_vagas.exceptions.UserFoundException;
+import br.com.jvneves.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.jvneves.gestao_vagas.modules.candidate.CandidateRepository;
+
+@Service
 public class CreateCandidateUseCase {
 
-  public CandidateEntity execute(CandidateEntity candidateEntity) {
+  @Autowired
+  private CandidateRepository candidateRepository;
 
+  public CandidateEntity execute(CandidateEntity candidateEntity) {
+    this.candidateRepository
+        .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+        .ifPresent((user) -> {
+          throw new UserFoundException();
+        });
+
+    return this.candidateRepository.save(candidateEntity);
   }
 
 }
