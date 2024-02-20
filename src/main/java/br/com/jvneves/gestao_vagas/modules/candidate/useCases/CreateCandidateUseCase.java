139ad56512cd1534de35e5efdccfac93c1ade113
João Vitor Neves <jvneves_marq@hotmail.com>
2024-02-20 11:36:42 -0300

    creating company authenticate

--- src/main/java/br/com/jvneves/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/jvneves/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,11 +1,13 @@
 package br.com.jvneves.gestao_vagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.jvneves.gestao_vagas.exceptions.UserFoundException;
 import br.com.jvneves.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.jvneves.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.jvneves.gestao_vagas.modules.company.entities.CompanyEntity;
 
 @Service
 public class CreateCandidateUseCase {
