318df32e5c588f709814ed68ffca781e76cd83c3
João Vitor Neves <jvneves_marq@hotmail.com>
2024-02-26 17:48:08 -0300

    finish refactoring, authenticated, tokens JWT and learning more about spring securit

--- src/main/java/br/com/jvneves/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/jvneves/gestao_vagas/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.security.Security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
   @Autowired
@@ -26,12 +28,12 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> {
           auth.requestMatchers("/candidate/").permitAll()
               .requestMatchers("/company/").permitAll()
-              .requestMatchers("/auth/company").permitAll()
+              .requestMatchers("/company/auth").permitAll()
               .requestMatchers("/candidate/auth").permitAll();
           auth.anyRequest().authenticated();
         })
-        .addFilterBefore(securityFilterChain, BasicAuthenticationFilter.class)
-        .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class);
+        .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
+        .addFilterBefore(securityFilterChain, BasicAuthenticationFilter.class);
     return http.build();
   }
 
